1789b409193f3829cfe01355b3d01497e2771c61
LN710Q <LN710Q@DEILABO1-PC24>
2018-06-26 11:32:56 -0600

    RE-ACTUALIZACION

---
+++ src/cortolabofinal/Cortolabofinal.java
@@ -0,0 +1,15 @@
+
+package cortolabofinal;
+
+import vista.Consulta;
+
+public class Cortolabofinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new Consulta().setVisible(true);
+    }
+    
+}
